8dc843868e50d844a0ee9a5b5a35d93a3656b6ec
Raj Bhut <rajbhut2832005@gmail.com>
2024-06-20 23:27:16 +0530

    room chat v1

--- api/index.js
+++ api/index.js
@@ -4,36 +4,38 @@
 // const { Server } = require("socket.io");
 // const cors = require("cors");
 import express from "express";
+const app = express();
 import http from "http";
 import { Server } from "socket.io";
-import cors from "cors";
-const app = express();
-app.use(cors());
-
 const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "http://localhost:5173",  
     methods: ["GET", "POST"],
   },
 });
 
+let clients = {};
+
 io.on("connection", (socket) => {
   console.log(`User Connected: ${socket.id}`);
+  clients[socket.id] = { id: socket.id };
 
+  socket.on("join personal", (data) => {
+    socket.join(data);
+    console.log(`User with id: ${socket.id} joined room: ${data}`);
+  });
 
-socket.on("join room" , (data) => {
-  socket.join(data)
-  console.log(`User with id: ${socket.id} joined room: ${data}`)
-})
+  socket.on("disconnect", () => {
+    delete clients[socket.id];
+    io.emit("updateClients", Object.values(clients));
+  });
 
-
-socket.on("send msg"  , (data) => {
-  
-  socket.to(data.room).emit("receive msg" , data.massage);
-})
-
+  socket.on("personal msg", (data) => {
+    socket.broadcast.to(data.room).emit("personal msg", data.message);
+    console.log(data.message);
+  });
 });
 
 server.listen(3001, () => {
